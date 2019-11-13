a28278bca74c923682a8b87edc7694a952d82847
ewabarczuk99 <ewa.barczuk@yahoo.pl>
2019-11-13 14:51:43 +0100

    first commit

---
+++ trello automated tests/trello automated tests/EnterPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace trello_automated_tests
+{
+	public class EnterPage 
+	{
+		IWebDriver driver;
+
+		[SetUp]
+		public void startBrowser()
+		{
+			driver = new ChromeDriver();
+		}
+
+		[Test]
+		public void test()
+		{
+			driver.Url = "https://trello.com";
+		}
+
+		[TearDown]
+		public void closeBrowser()
+		{
+			driver.Close();
+
+		}
+
+	}
+}
---
+++ trello automated tests/trello automated tests/EnterSignUpPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace trello_automated_tests
+{
+	class EnterSignUpPage
+	{
+		IWebDriver driver;
+
+		[SetUp]
+		public void startBrowser()
+		{
+			driver = new ChromeDriver();
+		}
+
+		[Test]
+		public void test()
+		{
+			driver.Url = "https://trello.com";
+			driver.FindElement(By.CssSelector("body > header > nav > div.float-right.buttons > a.btn.btn-sm.bg-white.font-weight-bold")).Click();
+			driver.Url = "https://trello.com/signup";
+
+		}
+
+		[TearDown]
+		public void closeBrowser()
+		{
+			driver.Close();
+
+		}
+
+	}
+}
